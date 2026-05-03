36d97d142dca24070c0ee9ee36ce097174db9f6e
Mihai Circea <mihai@circea.co>
2026-05-03 12:55:18 +0300

    release: restore clean v1.0.0 tree

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @author       Circea
  * @copyright    2026 Circea
  * @license      GPL-3.0-or-later
- * @version      1.0.1
+ * @version      1.0.0
  * @since        0.1.0 Added 2026-04-05
  * @link         https://circea.co
  */
